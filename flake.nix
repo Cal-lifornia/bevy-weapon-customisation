b34121fb0e1b908fc18dbce645b53a37b3855589
Cal-lifornia <76858909+Cal-lifornia@users.noreply.github.com>
2025-10-21 16:07:00 +1100

    Initial commit with flake.nix

---
+++ flake.nix
@@ -0,0 +1,84 @@
+{
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      self,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              clang
+              # lld is much faster at linking than the default Rust linker
+              lld
+            ];
+            buildInputs =
+              with pkgs;
+              [
+                # rust toolchain
+                # use rust-analyzer-nightly for better type inference
+                rust-analyzer
+                wgpu-utils
+                (rust-bin.stable.latest.default)
+              ]
+              # https://github.com/bevyengine/bevy/blob/v0.14.2/docs/linux_dependencies.md#nix
+              ++ (lib.optionals pkgs.stdenv.isLinux [
+                udev
+                alsa-lib
+                vulkan-loader
+                xorg.libX11
+                xorg.libXcursor
+                xorg.libXi
+                xorg.libXrandr # To use the x11 feature
+                libxkbcommon
+                wayland # To use the wayland feature
+              ])
+              ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
+                # https://discourse.nixos.org/t/the-darwin-sdks-have-been-updated/55295/1
+                apple-sdk_15
+              ]);
+
+          };
+          shellHook = ''
+            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
+              pkgs.lib.makeLibraryPath (
+                (with pkgs; [
+                  openssl
+                ])
+                ++ pkgs.lib.optionals pkgs.stdenv.isLinux (
+                  with pkgs;
+                  [
+
+                    alsaLib
+                    vulkan-loader
+                    alsa-lib
+                    libxkbcommon
+                    udev
+                  ]
+                )
+              )
+            }"
+          '';
+        };
+      }
+    );
+}
